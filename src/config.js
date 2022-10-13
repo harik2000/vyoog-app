b7dce1a59cd29133b2539188ec7b9e80933a9754
harik2000 <harikrishna.s2000@gmail.com>
2022-10-13 11:17:56 +0530

    second push

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+    cognito: {
+      REGION: "us-west-1",
+      USER_POOL_ID: "us-west-1_wmzKeQ6hs",
+      APP_CLIENT_ID: "4giin5pm8srmhvcbbq0t6dn415",
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Vyoog</h1>
+        <p>Cognito Test</p>
+      </div>
+    </div>
+  );
+}
